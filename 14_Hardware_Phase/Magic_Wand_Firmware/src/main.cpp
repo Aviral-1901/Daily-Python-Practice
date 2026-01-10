25306a1ac617d78f7aaf940bd3d552f5221a52b2
Aviral <adhikariaviral11@gmail.com>
2026-01-10 23:30:20 +0545

    Day 33: Implemented High Pass Filter and System Integration

--- 14_Hardware_Phase/Magic_Wand_Firmware/src/main.cpp
+++ 14_Hardware_Phase/Magic_Wand_Firmware/src/main.cpp
@@ -5,7 +5,59 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
+RingBuffer rbuffer;
+NeuralNetwork nn;
+Adafruit_MPU6050 my_mpu;
+float gravity = 0.0;
+float current_batch[50];
+float prediction = 0.0;
+
+void setup()
+{
+  Serial.begin(115200);
+  if(!my_mpu.begin())
+  {
+    while(1);
+  }
+  //to warm up the filter instantly
+  sensors_event_t a, g, t;
+  my_mpu.getEvent(&a, &g, &t);
+  gravity = a.acceleration.z; // Start at the correct valuel
+}
+
+void loop()
+{
+  sensors_event_t accel_data;
+  sensors_event_t gyro_data;
+  sensors_event_t temp_data;
+
+  my_mpu.getEvent(&accel_data, &gyro_data, &temp_data);
+  float raw_z = accel_data.acceleration.z;
+  float alpha = 0.1;
+  gravity = (0.1 * raw_z) + (0.9 * gravity);
+  float user_acc = raw_z - gravity;
+  user_acc /= 20;
+  rbuffer.add(user_acc);
+  if(rbuffer.is_ready())
+  {
+    rbuffer.get_batch(current_batch);
+    prediction = nn.predict(current_batch);
+    if(prediction > 0.8)
+    {
+      Serial.println("Walking |||||||||");
+    }
+    else
+    {
+      Serial.println("..............");
+    }
+  }
+  delay(10);
+}
+
+
+
 /* 
+what i did in day31 during learning
 RingBuffer rbuffer;
 NeuralNetwork nn;
 float current_batch[50];
@@ -31,6 +83,9 @@ void loop()
 }
 */
 
+
+/*
+what i did in day32 learning
 Adafruit_MPU6050 my_mpu;
 
 void setup()
@@ -54,3 +109,4 @@ void loop()
   Serial.println(accel_data.acceleration.z);
   delay(500);
 }
+*/
